dc2babfc988569c24d6887bb8bc0393b3c7510a8
feliposz <feliposz@gmail.com>
2024-04-09 04:30:34 -0300

    xxd: handling of grouping, seek, length, help, etc

--- xxd/ccxxd.go
+++ xxd/ccxxd.go
@@ -1,31 +1,69 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
+	"math"
 	"os"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprint(os.Stderr, "usage: ccxxd [filename]")
+
+	var octestPerLine, grouping, length, seekOffset int
+	var displayHelp bool
+
+	flag.IntVar(&octestPerLine, "c", 16, "format <cols> octets per line.")
+	flag.IntVar(&grouping, "g", 2, "number of octets per group in normal output.")
+	flag.IntVar(&length, "l", math.MaxInt, "stop after <len> octets.")
+	flag.IntVar(&seekOffset, "s", 0, "start at <seek> bytes absolute.")
+	flag.BoolVar(&displayHelp, "h", false, "print this summary.")
+	flag.Parse()
+
+	if octestPerLine < 1 || octestPerLine > 256 {
+		fmt.Fprintf(os.Stderr, "invalid value for -c (1-256)\n")
+		displayHelp = true
+	}
+
+	if grouping < 1 || grouping > 32 {
+		fmt.Fprintf(os.Stderr, "invalid value for -g (1-32)\n")
+		displayHelp = true
+	}
+
+	if seekOffset < 0 {
+		fmt.Fprintf(os.Stderr, "invalid value for -s (>= 0)\n")
+		displayHelp = true
+	}
+
+	if displayHelp || !flag.Parsed() {
+		flag.Usage()
 		os.Exit(1)
 	}
 
-	filename := os.Args[1]
+	var file *os.File
+	var err error
 
-	file, err := os.Open(filename)
+	if flag.NArg() > 0 {
+		file, err = os.Open(flag.Arg(0))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			os.Exit(2)
+		}
+		defer file.Close()
+	} else {
+		file = os.Stdin
+	}
+
+	offset := seekOffset
+	_, err = file.Seek(int64(seekOffset), io.SeekStart)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(2)
 	}
-	defer file.Close()
 
-	bytesPerRow := 16
-	grouping := 2
-	buffer := make([]byte, bytesPerRow)
-	offset := 0
-	for {
+	buffer := make([]byte, octestPerLine)
+	bytesWritten := 0
+	for bytesWritten < length {
 		bytesRead, err := file.Read(buffer)
 		if err != nil {
 			if err == io.EOF {
@@ -34,10 +72,12 @@ func main() {
 			fmt.Fprintf(os.Stderr, "%v\n", err)
 			os.Exit(2)
 		}
+		bytesLeft := length - bytesWritten
+		bytesRead = min(bytesRead, bytesLeft)
 
 		fmt.Printf("%08x:", offset)
 
-		for i := 0; i < bytesPerRow; i++ {
+		for i := 0; i < octestPerLine; i++ {
 			if i%grouping == 0 {
 				fmt.Print(" ")
 			}
@@ -50,7 +90,7 @@ func main() {
 
 		fmt.Print("  ")
 
-		for i := 0; i < bytesPerRow; i++ {
+		for i := 0; i < octestPerLine; i++ {
 			if i >= bytesRead {
 				fmt.Print(" ")
 			} else if buffer[i] < 32 || buffer[i] > 127 {
@@ -62,6 +102,7 @@ func main() {
 
 		fmt.Print("\n")
 
-		offset += bytesPerRow
+		offset += octestPerLine
+		bytesWritten += octestPerLine
 	}
 }
