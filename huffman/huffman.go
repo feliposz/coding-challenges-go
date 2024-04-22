d081b1cce43ef688a0b80e6c04eca22e164149dc
feliposz <feliposz@gmail.com>
2024-04-21 23:46:59 -0300

    huffman: build the binary tree from frequencies

--- huffman/huffman.go
+++ huffman/huffman.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 	"io"
 	"os"
@@ -28,14 +29,94 @@ func main() {
 		freq[b]++
 	}
 
-	for code, count := range freq {
-		printable := code
-		if code < 32 || code > 127 {
-			printable = '?'
-		}
-		fmt.Printf("%6d %c %02x  ", count, printable, code)
-		if (code+1)%8 == 0 {
-			fmt.Println()
+	debugFreqTable := false
+
+	if debugFreqTable {
+		for code, count := range freq {
+			printable := code
+			if code < 32 || code > 127 {
+				printable = '?'
+			}
+			fmt.Printf("%6d %c %02x  ", count, printable, code)
+			if (code+1)%8 == 0 {
+				fmt.Println()
+			}
 		}
 	}
+
+	hnHeap := &HuffNodeHeap{}
+
+	heap.Init(hnHeap)
+
+	for code, count := range freq {
+		if count > 0 {
+			heap.Push(hnHeap, &HuffNode{count, byte(code), nil, nil})
+		}
+	}
+
+	for len(*hnHeap) > 1 {
+		left := heap.Pop(hnHeap).(*HuffNode)
+		right := heap.Pop(hnHeap).(*HuffNode)
+		heap.Push(hnHeap, &HuffNode{left.Weight + right.Weight, 0, left, right})
+	}
+
+	huffTree := heap.Pop(hnHeap).(*HuffNode)
+
+	debugHuffTree := true
+	if debugHuffTree {
+		printTree(huffTree, 0)
+	}
+}
+
+func printTree(node *HuffNode, depth int) {
+	if node == nil {
+		return
+	}
+	for i := 0; i < depth; i++ {
+		fmt.Print("    ")
+	}
+	if node.Code == 0 {
+		fmt.Printf("node weight:%d\n", node.Weight)
+	} else {
+		printable := node.Code
+		if printable < 32 || printable > 127 {
+			printable = '?'
+		}
+		fmt.Printf("char:'%c' code:%02x weight:%d\n", printable, node.Code, node.Weight)
+	}
+	printTree(node.Left, depth+1)
+	printTree(node.Right, depth+1)
+}
+
+type HuffNode struct {
+	Weight int
+	Code   byte
+	Left   *HuffNode
+	Right  *HuffNode
+}
+
+type HuffNodeHeap []*HuffNode
+
+func (h HuffNodeHeap) Len() int {
+	return len(h)
+}
+
+func (h HuffNodeHeap) Less(a, b int) bool {
+	return h[a].Weight < h[b].Weight
+}
+
+func (h HuffNodeHeap) Swap(a, b int) {
+	h[a], h[b] = h[b], h[a]
+}
+
+func (h *HuffNodeHeap) Push(x any) {
+	*h = append(*h, x.(*HuffNode))
+}
+
+func (h *HuffNodeHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
 }
